0a8be7b112c280e90d2b3952a42ca107709b3720
Juan Severiano <111772146+Juan-Severiano@users.noreply.github.com>
2023-07-19 00:25:22 +0000

    Finalmente cnectado a API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const path = require('path');
 
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -13,6 +14,15 @@ app.set('view engine', 'ejs');
 
 app.use(routes);
 
+app.get('/characters', async (req, res) => {
+  try {
+    const characters = await getCharacter();
+    res.send(characters.results);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Erro ao obter os personagens de Rick and Morty.');
+  }
+});
 
 app.listen(3000, () => {
   console.log('Acessar http://localhost:3000');
--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,12 @@
-exports.home = (req, res) => {
-    res.render('index')
+const plumbus = require('rickmortyapi');
+
+exports.home = async (req, res) => {
+    try {
+        const c = await plumbus.getCharacters();
+        res.send(c.data.results)
+        res.render('index', {c});
+    } catch (error) {
+        console.log(error);
+        res.status(500).render('errorAPI');
+    }
 }
