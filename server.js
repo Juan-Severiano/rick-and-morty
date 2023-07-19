60f246523fe56df4d1e97a8d379b8f9de996bb50
Juan Severiano <111772146+Juan-Severiano@users.noreply.github.com>
2023-07-19 20:27:58 +0000

    header completo

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'frontend')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
