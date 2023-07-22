830994b907827443f9053bb2c239ba8d3b38caf1
juan <juansoussev@gmail.com>
2023-07-22 17:35:07 -0300

    coisas p deploy4

--- src/controllers/characters.js
+++ src/controllers/characters.js
@@ -1,16 +1,16 @@
-const { async } = require('regenerator-runtime');
-const plumbus = require('rickmortyapi');
+const axios = require('axios');
 
 exports.characters = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const charactersAll = await plumbus.getCharacters({ page });
-    const characters = charactersAll.data.results;
+    const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${page}`);
+    const characters = response.data.results;
 
     for (const c of characters) {
       const firstEpisodeName = await getFirstEpisodeName2(c.name, characters);
       c.firstEpisodeName = firstEpisodeName;
     }
+
     res.render('charactersList', { characters, page });
   } catch (error) {
     console.error('Ocorreu um erro:', error);
@@ -28,11 +28,11 @@ const getFirstEpisodeName2 = async (characterName, charactersList) => {
     }
     const firstEpisodeId = character.episode[0].split('/').pop();
 
-    const episodeResponse = await plumbus.getEpisode(parseInt(firstEpisodeId));
+    const episodeResponse = await axios.get(`https://rickandmortyapi.com/api/episode/${firstEpisodeId}`);
     const firstEpisodeName = episodeResponse.data.name;
 
     return String(firstEpisodeName);
   } catch (error) {
     console.error('Ocorreu um erro ao obter informações do personagem:', error.message);
   }
-}
+};
