306128df1f418009eb50df46227d36ec510bd92f
juan <juansoussev@gmail.com>
2023-07-22 14:41:55 -0300

    EpisodeController Concluida

--- routes.js
+++ routes.js
@@ -3,14 +3,16 @@ const route = express.Router();
 const home = require('./src/controllers/home')
 const characters = require('./src/controllers/characters')
 const locations = require('./src/controllers/locations')
+const episodes = require('./src/controllers/episodes')
 
 // Rotas da home
 route.get('/', home.home);
-route.post('/', home.search)
 
 route.get('/characters', characters.characters)
 
 route.get('/locations', locations.locationController)
+
+route.get('/episodes', episodes.episodeController)
 
 
 module.exports = route;
---
+++ src/controllers/episodes.js
@@ -0,0 +1,14 @@
+const plumbus = require('rickmortyapi');
+
+exports.episodeController = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const episodesAll = await plumbus.getEpisodes({ page })
+    const episodes = episodesAll.data.results
+
+    res.render('episodeList', { episodes, page });
+  } catch (error) {
+    console.log(error)
+    res.status(500).render('errorAPI');
+  }
+}
