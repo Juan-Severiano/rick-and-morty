59f2d8ffac7a9e724b6e832855b61cbcba47f19d
juan <juansoussev@gmail.com>
2023-07-22 14:24:04 -0300

    LocationController concluida

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,9 +1,14 @@
 const plumbus = require('rickmortyapi');
 
-exports.locationController = async () => {
+exports.locationController = async (req, res) => {
   try {
-    const locations = await getLocations()
+    const page = parseInt(req.query.page) || 1;
+    const locationsAll = await plumbus.getLocations({ page })
+    const locations = locationsAll.data.results
+
+    res.render('locationList', { locations, page });
   } catch (error) {
     console.log(error)
+    res.status(500).render('errorAPI');
   }
 }
