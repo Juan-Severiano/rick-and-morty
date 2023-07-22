a3d51201b24974f19ce91167deb3fd8bc912b506
juan <juansoussev@gmail.com>
2023-07-22 16:49:20 -0300

    coisas p deploy

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express();
 const routes = require('./routes');
 const path = require('path');
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
@@ -14,16 +12,6 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(routes);
-
-app.get('/characters', async (req, res) => {
-  try {
-    const characters = await getCharacter();
-    res.send(characters.results);
-  } catch (error) {
-    console.log(error);
-    res.status(500).send('Erro ao obter os personagens de Rick and Morty.');
-  }
-});
 
 app.listen(3000, () => {
   console.log('Acessar http://localhost:3000');
